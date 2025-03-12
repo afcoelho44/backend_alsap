d9805b21e6d08230fc9026a063c1a4618b542c19
Ana Fábia Coelho <afcoelho44@gmail.com>
2025-03-12 12:13:21 -0300

    Initial commit

---
+++ src/main/java/udesc/br/alsap/AlsapApplication.java
@@ -0,0 +1,13 @@
+package udesc.br.alsap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlsapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udesc/br/alsap/AlsapApplicationTests.java
@@ -0,0 +1,13 @@
+package udesc.br.alsap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
