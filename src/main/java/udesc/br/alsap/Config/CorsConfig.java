a4633879eeba1f05168f60f94b0f0cdbe5cea038
Ana Fábia Coelho <afcoelho44@gmail.com>
2025-05-18 22:20:00 -0300

    Ajustando o docker e o cors

--- src/main/java/udesc/br/alsap/Config/CorsConfig.java
+++ src/main/java/udesc/br/alsap/Config/CorsConfig.java
@@ -1,4 +1,4 @@
-package udesc.br.alsap.Config;
+package udesc.br.alsap.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/") // porta do seu frontend Vite/React
+                .allowedOrigins("*") 
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
